0e23177564fbbdc466e638fb0557b210e1bba370
Soupbubbles <vattentrollet@gmail.com>
2021-09-20 23:22:29 +0200

    Added images, fixed transparent background

--- ImageGen.java
+++ ImageGen.java
@@ -15,29 +15,28 @@ public class ImageGen {
 		imgName = name;
 	}
 
-	public static void main(String[] args) throws IOException {		
+	public static void main(String[] args) throws IOException {
 		if (args.length < 1) {
 			System.err.println("Error: No image name provided");
 			return;
-		}
-		else if (args.length > 1) {
+		} else if (args.length > 1) {
 			System.err.println("Error: Too many arguments");
 			return;
 		}
-		
+
 		ImageGen imgGen = new ImageGen(args[0]);
 		imgGen.genImage();
 	}
 
 	public void genImage() throws IOException {
-		File file = new File(imgName);
+		File file = new File("images/" + imgName);
 		BufferedImage image = ImageIO.read(file);
-		
+
 		if (image.getHeight() != 8 && image.getWidth() != 8) {
 			System.err.println("Error: Image is not 8x8");
 			return;
 		}
-		
+
 		Color[][] matrix = new Color[8][8];
 
 		for (int x = 0; x < 8; x++) {
@@ -45,29 +44,32 @@ public class ImageGen {
 				matrix[x][y] = new Color(image.getRGB(x, y));
 			}
 		}
-		
+
 		String output = "from sense_hat import SenseHat\n\n\nsense = SenseHat()\n\n\nimage_pixels = [\n\n";
-		
+
 		for (int y = 0; y < 8; y++) {
 			for (int x = 0; x < 8; x++) {
 				Color color = matrix[x][y];
-				output += "(" + color.getRed() + ", " + color.getGreen() + ", " + color.getBlue() + ")";
-				
+
+				if (color.getAlpha() != 0) {
+					output += "(" + color.getRed() + ", " + color.getGreen() + ", " + color.getBlue() + ")";
+				} else {
+					output += "(0, 0, 0)";
+				}
+
 				if (x == 7) {
 					output += ",\n";
-				}
-				else {
+				} else {
 					output += ", ";
 				}
 			}
 		}
-		
-		
+
 		output = output.substring(0, output.length() - 2);
 		output += "\n\n]\n\nsense.set_pixels(image_pixels)";
 
 		try (PrintStream ps = new PrintStream(new FileOutputStream(imgName.replace(".png", "").replace(".jpg", "") + ".py"))) {
-		    ps.print(output);
+			ps.print(output);
 		}
 	}
 }
